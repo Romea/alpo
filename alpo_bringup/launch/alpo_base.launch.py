27fe099b35bd7e2aee03639fc7271075ad379ef6
jean.laneurit <jean.laneurit@inrae.fr>
2023-04-11 17:55:20 +0200

    bringup: remove some screen output

--- alpo_bringup/launch/alpo_base.launch.py
+++ alpo_bringup/launch/alpo_base.launch.py
@@ -57,21 +57,29 @@ def launch_setup(context, *args, **kwargs):
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        output="screen",
         parameters=[robot_description],
+        output={
+            'stdout': 'log',
+            'stderr': 'log',
+        }
     )
 
     spawn_entity = Node(
         condition=LaunchConfigurationEquals("mode", "simulation"),
         package="gazebo_ros",
         executable="spawn_entity.py",
+        exec_name="gazebo_spawn_entity.py",
         arguments=[
             "-topic",
             robot_description_name,
             "-entity",
             robot_namespace,
         ],
-        output="screen",
+        output={
+            'stdout': 'log',
+            'stderr': 'log',
+        }
+
     )
 
     alpo_bridge = Node(
@@ -86,7 +94,7 @@ def launch_setup(context, *args, **kwargs):
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[robot_description, controller_manager_yaml_file],
-        output="screen",
+        # output="screen",
     )
 
     controller = IncludeLaunchDescription(
