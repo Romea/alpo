61ffbef8a809ae11a1faa2cbd4fd61859ea5817c
jean.laneurit <jean.laneurit@inrae.fr>
2023-04-14 15:18:09 +0200

    remove docker

--- alpo_bridge_docker/ros_entrypoint_ros1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# unsetting ROS_DISTRO to silence ROS_DISTRO override warning
-unset ROS_DISTRO
-# setup ros1 environment
-source "/opt/ros/$ROS1_DISTRO/setup.bash"
-
-exec "$@"
