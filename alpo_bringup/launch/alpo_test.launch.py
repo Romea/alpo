fc5a4e37e4d85c95954fa5ebd5a1129965122ec7
Jean Laneurit <jean.laneurit@inrae.fr>
2023-10-13 16:03:45 +0200

    bringup: add test launch file
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- alpo_bringup/launch/alpo_test.launch.py
+++ alpo_bringup/launch/alpo_test.launch.py
@@ -133,11 +133,10 @@ def launch_setup(context, *args, **kwargs):
 
                 IncludeLaunchDescription(
                     PythonLaunchDescriptionSource(
-                        get_package_share_directory("romea_teleop_drivers")
-                        + "/launch/teleop.launch.py"
+                        get_package_share_directory("alpo_bringup")
+                        + "/launch/alpo_teleop.launch.py"
                     ),
                     launch_arguments={
-                        "robot_type": "alpo",
                         "robot_model": robot_model,
                         "joystick_type": joystick_type,
                         "joystick_driver": "joy",
